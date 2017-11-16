ce2d9139e93ac1379d78ee71f743eb2468c0d131
Unknown <cef363@nyu.edu>
2017-11-16 11:40:04 -0500

    working on light switch but not done

---
+++ Assets/Scripts/LightSwitch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour {
+	public GameObject lamp;
+	private bool on = false;
+
+	//put the stuff so the UI text only shows up when you're looking at it
+	//this is probably wrong?
+	void OnTriggerStay(Collider plyr) {
+		if (plyr.tag == "Player" && Input.GetMouseButton(0) && !on)
+		{
+			lamp.SetActive (true);
+			on = true;
+		}
+		else if (plyr.tag == "Player" && Input.GetMouseButton(0) && on)
+		{
+			lamp.SetActive (false);
+			on = false;
+		}
+	}
+}
