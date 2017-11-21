a238a6aed7555ddda249db42f779faa7c4182b1c
Unknown <kcp320@nyu.edu>
2017-11-21 09:49:37 -0500

    2D Examination and Lights

--- Assets/Scripts/ObjectInteraction.cs
+++ Assets/Scripts/ObjectInteraction.cs
@@ -8,7 +8,8 @@ using UnityEngine;
 public class ObjectInteraction : MonoBehaviour {
 
 	public bool hitObject;
-	public bool hitLamp;
+	public bool clickedLamp=false;
+	public bool examining2D = false;
 	public GameObject heldObject;
 	public GameObject heldObjectPosition; //position of the object when it's being held
 	public GameObject examineObjectPosition; //position of the object when it's being examined
@@ -23,6 +24,8 @@ public class ObjectInteraction : MonoBehaviour {
 	//bools for UI Manager
 	public bool lookingAtOrigin=false;
 	public bool lookingAtInteractiveObject=false;
+	public bool lookingAtInteractive2D=false;
+	public string ObjectName;
 
 	float mouseSensitivity = 50f;
 
@@ -35,6 +38,9 @@ public class ObjectInteraction : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		//reset clickedLamp bool
+		clickedLamp=false;
+
 		//define a Ray variable, extend from the camera's forward
 		Ray playerRay = new Ray (Camera.main.transform.position, Camera.main.transform.forward);
 
@@ -59,7 +65,10 @@ public class ObjectInteraction : MonoBehaviour {
 //RAYCAST CHECKER FOR UI
 		if (Physics.Raycast (playerRay, out rayHit, maxRayDistance)) { //if true == an object has been hit
 			if (heldObject != null) {
-				if (rayHit.collider.gameObject == objectOriginScript.OriginCollider) { //if the player is looking at the object's origin
+				if(heldObject.tag=="Interactive2D"){
+					//do nothing
+				}
+				else if (rayHit.collider.gameObject == objectOriginScript.OriginCollider) { //if the player is looking at the object's origin
 					lookingAtOrigin = true;
 				} else {
 					lookingAtOrigin = false;
@@ -68,14 +77,45 @@ public class ObjectInteraction : MonoBehaviour {
 			{
 				if (rayHit.collider.gameObject.CompareTag ("InteractiveObject")) { //if true == the hit object has a tag of "InteractiveObject"
 					lookingAtInteractiveObject = true;
+					ObjectName = rayHit.collider.gameObject.name;
+				} else if (rayHit.collider.gameObject.CompareTag ("Interactive2D")) {
+					lookingAtInteractive2D = true;
+					ObjectName = rayHit.collider.gameObject.name;
 				} else {
 					lookingAtInteractiveObject = false;
+					lookingAtInteractive2D = false;
 				}
 			}
 		}
 
 		if (Input.GetMouseButtonDown (0)) { //if left mouse button clicked
-			if (heldObject != null) { //if there's something in the heldObject slot
+			//2D EXAMINATION
+			if ((Physics.Raycast (playerRay, out rayHit, maxRayDistance)) && (rayHit.collider.gameObject.CompareTag ("Interactive2D"))) { //if true == a 2D object has been hit
+				if (examining2D == false) {
+					examining2D = true;
+
+					//Essentially Pickup function modified for non-physics
+					heldObject = rayHit.collider.gameObject;
+					objectOriginScript = rayHit.collider.gameObject.GetComponent<ObjectOrigin> ();
+					heldObjectOriginPos = rayHit.collider.gameObject.transform.position;
+					heldObjectOriginRot = rayHit.collider.gameObject.transform.eulerAngles;
+
+					thisOriginalPlayerController.enabled = false;
+					thisCameraControllerPlayer.enabled = false;
+					thisCameraControllerCamera.enabled = false;
+					//display image either here or in UI
+					rayHit.collider.gameObject.transform.position = examineObjectPosition.transform.position;
+					rayHit.collider.gameObject.transform.up = Camera.main.transform.forward;
+				} else if (examining2D == true) {
+					examining2D = false;
+					heldObject = null; //clear the heldObject slot
+					thisOriginalPlayerController.enabled = true;
+					thisCameraControllerPlayer.enabled = true;
+					thisCameraControllerCamera.enabled = true;
+					rayHit.collider.gameObject.transform.position = heldObjectOriginPos;
+					rayHit.collider.gameObject.transform.eulerAngles = heldObjectOriginRot;
+				}
+			} else if (heldObject != null) { //if there's something in the heldObject slot
 				heldObject.transform.SetParent (null); //unparent the object from the player
 				if (Physics.Raycast (playerRay, out rayHit, maxRayDistance)) { //if true == an object has been hit
 //PUTBACK OBJECT
@@ -94,23 +134,22 @@ public class ObjectInteraction : MonoBehaviour {
 					heldObject = null; //clear the heldObject slot
 				}
 				
-			} else { //if there's no object in the heldObject slot
+			} else { //if there's no object in the heldObject slot and left mouse button clicked
 				if (Physics.Raycast (playerRay, out rayHit, maxRayDistance)) { //if true == an object has been hit
 //PICKUP OBJECT
 					if (rayHit.collider.gameObject.CompareTag ("InteractiveObject")) { //if true == the hit object has a tag of "InteractiveObject"
-						if(rayHit.collider.gameObject.name==("Lamp")){
-							
-							}
-						PickupObj (rayHit.collider.gameObject); //run pickup function
+						if (rayHit.collider.gameObject.name == ("Lamp")) {
+							clickedLamp = true;
+						} else {
+							PickupObj (rayHit.collider.gameObject); //run pickup function
+						}
 					}
-				} else { // Raycast failed, so there's no object in raycast
-					//do nothing
 				}
 			}
 		}
 
 //OBJECT EXAMINATION
-		if (heldObject != null) { //if there's something in the heldObject slot
+		if (heldObject != null&&heldObject.tag=="InteractiveObject") { //if there's something in the heldObject slot
 
 			if (Input.GetMouseButton (1) || Input.GetKey (KeyCode.LeftShift)) { //if right mouse or left shift held
 			
