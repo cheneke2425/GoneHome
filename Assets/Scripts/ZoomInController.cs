2fbae6b0ad436d5274e79c2eeac42b66b092b198
Unknown <qq265@nyu.edu>
2017-11-21 11:53:07 -0500

    ZoomIn Controller
    Need to be smooth a bit more

---
+++ Assets/Scripts/ZoomInController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomInController : MonoBehaviour {
+	//public Transform myCamera;
+	//[Range(0f, 1f)]
+	public float velocitySmooth = 0f;
+	//[Range(0f, 1f)]
+	public float timeSmooth = 0.3f;
+
+	public float defaultFieldOfView = 60f;
+	public float zoomInFieldOfView = 27f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift) || Input.GetMouseButton(1)){
+			Debug.Log("ZOOM INNNNNNN");
+			SmoothCamZoom (zoomInFieldOfView, defaultFieldOfView);
+		} else {
+			SmoothCamZoom (defaultFieldOfView, zoomInFieldOfView);
+		}
+
+	}
+
+	public void SmoothCamZoom(float originView, float targetView){
+		transform.GetComponent<Camera> ().fieldOfView = Mathf.SmoothDamp(originView, targetView, ref velocitySmooth, timeSmooth);
+		//transform.GetComponent<Camera>().fieldOfView = Mathf.Lerp(originView, targetView, timeSmooth);
+	}
+}
